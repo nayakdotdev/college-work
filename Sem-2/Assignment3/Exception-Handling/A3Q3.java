3651e93efd022883e5a58ca94dd84905f3188e14
Sanket <sanketsemail706@gmail.com>
2026-04-11 22:21:19 +0530

    add problem statement to A3Q12

--- Sem-2/Assignment3/Exception-Handling/A3Q3.java
+++ Sem-2/Assignment3/Exception-Handling/A3Q3.java
@@ -1,6 +1,7 @@
 /*
 Problem Statement:
-
+Create a class Student & enter mark, name of the student. If mark is more than 100, create
+exception MarksOutOfBoundException & throw it using Java.
 */
 
 import java.util.Scanner;
